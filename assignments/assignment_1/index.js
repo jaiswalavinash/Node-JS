b6eb054b2f52a9e64577d8ddb0190ac51fd702c4
Avinash Kumar <96715572+jaiswalavinash@users.noreply.github.com>
2022-02-23 12:31:22 +0530

    My commit for Assignment-1

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,17 +1,31 @@
+const { rawListeners } = require("process");
+
 function getNameFromCommandLine() {
-    // Write you code here, name should be taken as args in process.argv
+  // Write you code here, name should be taken as args in process.argv
+  const firstName = process.argv[2];
+  return firstName;
 }
 
 function getNameFromEnv() {
-    // Write your code here
+  // Write your code here
+  return process.env.name;
 }
 
 function getNameFromReadLine() {
-    // Write your code here
+  // Write your code here
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question((ans) => {
+    console.log(ans);
+    rl.close();
+  });
 }
 
 module.exports = {
-    getNameFromCommandLine,
-    getNameFromEnv,
-    getNameFromReadLine
-}
+  getNameFromCommandLine,
+  getNameFromEnv,
+  getNameFromReadLine,
+};
